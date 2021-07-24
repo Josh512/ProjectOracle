10d63e983aeeadc32bddc4db786a51ef2b4fa49d
Josh512 <j.aurella512@gmail.com>
2021-07-24 12:19:02 -0700

    Script that will scan a target and provide services/versions.

---
+++ service_finder.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import nmap3
+
+ip = input('Enter Target IP Address: ')
+nmap = nmap3.Nmap()
+results = nmap.nmap_version_detection(ip)
+
+for i in results[ip]['ports']:
+    try:
+        print(i['service']['product'] + '/' + i['service']['version'])
+    except:
+        pass
